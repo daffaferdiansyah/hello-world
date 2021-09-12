7299482d2da6e52775c884ac13cb0cbfa1df37b7
Muhammad Daffa Ferdiansyah <85597705+daffaferdiansyah@users.noreply.github.com>
2021-09-12 14:15:53 +0700

    Add files via upload

---
+++ no 1.py
@@ -0,0 +1,13 @@
+list_bilangan = list(map(int, input().split(" ")))
+
+def printList():
+    for x in list_bilangan:
+        if x % 2 != 0:
+            print(x, end = " ")
+    print()
+
+printList()
+#panggil fungsi printList()
+list_bilangan[0] += 1
+#panggil fungsi printList
+printList()
